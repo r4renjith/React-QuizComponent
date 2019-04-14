c8f7c18512e0dbbaa2d6e42c2049e89be9c91fbd
r4renjith <r4renjith@gmail.com>
2019-04-14 23:37:39 +0300

    SingleDiv

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,8 +9,7 @@ class Quiz extends Component{
     }
     render(){
         return(
-            <div><div classname="QuizQuestion">{quizData.quiz_questions[0]
-            .instruction_text}</div></div>
+            <div>Quiz</div>
         )
     }
 }
