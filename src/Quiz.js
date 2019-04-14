bf07463ad175129b2820dd2179f79dacbcd07c84
r4renjith <r4renjith@gmail.com>
2019-04-14 23:38:42 +0300

    With ClassName

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,7 +9,8 @@ class Quiz extends Component{
     }
     render(){
         return(
-            <div>Quiz</div>
+            <div><div classname="QuizQuestion">{quizData.quiz_questions[0]
+            .instruction_text}</div></div>
         )
     }
 }
